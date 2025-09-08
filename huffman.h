4118345b505f05510bc7279b9bbe4614148d2375
Hector_AG <colorlesshallway.faded@gmail.com>
2025-09-08 09:16:49 -0600

    Agregue .h archivo para huffman.cpp

---
+++ huffman.h
@@ -0,0 +1,34 @@
+// Huffman.h
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <iomanip>
+
+
+struct Nodo {
+    char caracter;
+    int frecuencia;
+    Nodo* izquierda;
+    Nodo* derecha;
+
+    Nodo(char c, int f);
+    Nodo(Nodo* l, Nodo* r);
+};
+
+struct Comparador {
+    bool operator()(Nodo* a, Nodo* b);
+};
+
+Nodo* construirArbolHuffman(const std::string& texto);
+void generarCodigos(Nodo* nodo, const std::string& codigo, std::unordered_map<char, std::string>& codigos);
+int longitudCodificada(const std::string& texto, const std::unordered_map<char, std::string>& codigos);
+double longitudPromedioEsperada(const std::string& texto, const std::unordered_map<char, std::string>& codigos);
+void analizarConHuffman(const std::string& nombreTransmision, const std::vector<std::string>& archivosMcode);
+
+#endif
